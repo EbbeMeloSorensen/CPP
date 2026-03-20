eeab920aa2d7738ee6f79f42a1ca120f1ee10a77
Ebbe Melo Sørensen <ebs@dmi.dk>
2026-03-20 15:59:00 +0100

    -

--- MyStaticLib3/tests/math_tests.cpp
+++ MyStaticLib3/tests/math_tests.cpp
@@ -8,7 +8,7 @@ TEST(AddiwwtionTests, AddsPosiwwtiveNumbers)
 {
     CDoubleHeap heap1;
     int size = heap1.GetSize();
-    size = 2; // Make the test fail
+    //size = 2; // Make the test fail -> yup it is being catched by teamcity using CMake test
     
     EXPECT_EQ(0, size);
 }
